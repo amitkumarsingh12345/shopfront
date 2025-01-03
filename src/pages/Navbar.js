f60fa9b8f333be37723cdb8dc65f5016bd08d12d
omware12345 <omwareinfosystem@gmail.com>
2025-01-02 21:38:12 -0800

    THIRD

--- src/pages/Navbar.js
+++ src/pages/Navbar.js
@@ -79,10 +79,10 @@ const Navbar = () => {
                         <nav className="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-0">
                             <NavLink to="" className="text-decoration-none d-block d-lg-none">
                                 <span className="h1 text-uppercase text-dark bg-light px-2">
-                                    Multi
+                                    DIVYA
                                 </span>
                                 <span className="h1 text-uppercase text-light bg-primary px-2 ml-n1">
-                                    Shop
+                                    SHOP
                                 </span>
                             </NavLink>
                             <button
